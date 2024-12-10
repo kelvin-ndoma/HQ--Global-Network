63017d868892b168042c7c2053c399ea80e69499
Kelvin Ndoma <kelvinndomamutua@gmail.com>
2024-12-10 17:51:29 +0300

    getting ready for active storage

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://127.0.0.1:5174"  # Frontend URL
+    origins "http://127.0.0.1:5173"  # Frontend URL
 
     resource "*",
       headers: :any,
